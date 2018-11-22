3a007c2463c547baa46c8d3f8b743177d72afef0
Jordan Larios <jolarios96@gmail.com>
2018-11-21 16:07:42 -0800

    beginning api call function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 var topics = ['games', 'coffee', 'coding', 'music', 'penguins', 'deer', 'confusion'];
 
-for (var i = 0; i < topics.length; i++){
+for (var i = 0; i < topics.length; i++) {
     var newElement = $('<button>');
-    newElement.attr( 'data-topic', topics[i]);
+    newElement.attr('data-topic', topics[i]);
     newElement.addClass('topic-btn');
     newElement.text(topics[i]);
     $('#btn-wrapper').append(newElement);
@@ -10,7 +10,7 @@ for (var i = 0; i < topics.length; i++){
 }
 
 // Goal: inside element of '#btn-wrapper, on click btn of class='topic-btn', get button's id
-$('#btn-wrapper').on('click', '.topic-btn', function(){
+$('#btn-wrapper').on('click', '.topic-btn', function () {
     console.log("Requesting GIFS of topic: " + $(this).data('topic'));
 
     getGifs($(this).data('topic'));
@@ -24,6 +24,16 @@ $('#btn-wrapper').on('click', '.topic-btn', function(){
 // ##       ##     ## ##   ### ##    ##    ##     ##  ##     ## ##   ### ##    ## 
 // ##        #######  ##    ##  ######     ##    ####  #######  ##    ##  ######  
 
-function getGifs(topic){
+function getGifs(topic) {
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q='
+        + topic
+        + '&api_key=pDexbJg488nIkdEvMYtFMAflgSFFYRLA';
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response){
+            
+        })
 }
