7eee79d786295ffde6a9bf89cc791bb24613cd5c
Jordan Larios <jolarios96@gmail.com>
2018-11-21 19:00:03 -0800

    added click event that plays/pauses gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topics = ['games', 'coffee', 'coding', 'music', 'penguins', 'deer', 'confusion'];
+var topics = ['hamster', 'games', 'coffee', 'coding', 'music', 'penguins', 'deer', 'confusion'];
 
 for (var i = 0; i < topics.length; i++) {
     var newElement = $('<button>');
@@ -14,7 +14,23 @@ $('#btn-wrapper').on('click', '.topic-btn', function () {
     console.log("Requesting GIFS of topic: " + $(this).data('topic'));
 
     getGifs($(this).data('topic'));
-})
+});
+
+$('#gif-wrapper').on('click', '.gif-preview', function () {
+    var tmp;
+
+    console.log('stored original src in tmp');
+    tmp = $(this).attr('src');
+
+    console.log('overwriting original src with data("src")');
+    $(this).attr('src', $(this).data('src'));
+    
+    console.log('storing tmp in data("src")')
+    $(this).data('src', tmp);
+    console.log ('data = ' + $(this).data('src'));
+
+
+});
 
 // ######## ##     ## ##    ##  ######  ######## ####  #######  ##    ##  ######  
 // ##       ##     ## ###   ## ##    ##    ##     ##  ##     ## ###   ## ##    ## 
@@ -25,9 +41,12 @@ $('#btn-wrapper').on('click', '.topic-btn', function () {
 // ##        #######  ##    ##  ######     ##    ####  #######  ##    ##  ######  
 
 function getGifs(topic) {
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q='
-        + topic
-        + '&limit=10_&api_key=pDexbJg488nIkdEvMYtFMAflgSFFYRLA';
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?'
+        + 'q=' + topic
+        + '&limit=10'
+        + '&lang = en'
+        + '&api_key=TQzeL4gjMxH9FaCG1Bl4ywCXFOxyTl6Y'
+        ;
 
     $.ajax({
         url: queryURL,
@@ -36,22 +55,31 @@ function getGifs(topic) {
         .then(function (response) {
             var results = response.data;
 
-            for (var i = 0; i < 10; i++) {
-
+            for (var i = 0; i < results.length; i++) {
                 var rating = results[i].rating;
                 var p = $('<p>').text('Rating: ' + rating);
 
                 var newImg = $('<img>');
-                newImg.attr('src', results[i].images.fixed_height.url);
+                newImg.attr('src', results[i].images.fixed_height_still.url);
+                newImg.attr('data-src', results[i].images.fixed_height.url)
+                newImg.attr('id', 'img-' + i);
+                newImg.addClass('gif-preview')
 
                 var newDiv = $('<div>');
+                newDiv.addClass('gif-container');
                 newDiv.append(p);
                 newDiv.append(newImg);
 
                 $('#gif-wrapper').append(newDiv);
 
-                console.log('Gif-' + (i + 1) + ' Get!');
+                console.log('Gif-' + i + ' Get!');
             }
+
             console.log(response.data);
         })
+
+    // .then(function (response) {
+    //     console.log(response.data);
+
+    // })
 }
